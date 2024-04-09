7cb4cced0aaed01d1fd43ef61ec2939eb029ab62
Elif <elif.keskin233@gmail.com>
2024-04-09 17:02:26 +0300

    first project

---
+++ Backend/CleanArchitecture/CleanArchitecture.Application/Features/Products/Queries/GetAllProducts/GetAllProductsParameter.cs
@@ -0,0 +1,9 @@
+﻿using CleanArchitecture.Core.Filters;
+
+namespace CleanArchitecture.Core.Features.Users.Queries.GetAllProducts
+{
+    public class GetAllProductsParameter : RequestParameter
+    {
+
+    }
+}
